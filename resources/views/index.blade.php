8dd3bca77041a7f59851c924c79143d9b7413364
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2023-01-06 22:04:52 +0800

    登入頁面

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('page-title', '登入')
+
+@section('page-content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">登入</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">登入</li>
+    </ol>
+
+</div>
+@endsection
