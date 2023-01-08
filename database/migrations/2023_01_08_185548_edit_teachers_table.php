013bb01d24a928be8676558590b59e0246928c26
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2023-01-09 02:58:38 +0800

    修改teachers table

---
+++ database/migrations/2023_01_08_185548_edit_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teachers', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('account');
+            $table->dropColumn('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teachers', function (Blueprint $table) {
+            $table->string('name',20);
+            $table->string('account',20);
+            $table->string('password',20);
+        });
+    }
+};
