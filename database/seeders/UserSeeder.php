8b3e98c3d5716ec4bd0aff09b1a33ca1894a8bae
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2023-01-09 03:22:03 +0800

    設定管理員及教師預設帳號

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,13 +19,13 @@ class UserSeeder extends Seeder
     {
         User::truncate();
         Student::truncate();
-//        User::create(
-//            [//管理員
-//                'type'=>0,
-//                'name'=>'admin',
-//                'email'=>'admin@gmail.com',
-//                'password'=>'$2y$10$EfHSnowZ1S0gsGJjVV4v9.yXBKH9SSfGGvFgghVi2DtUasju/UQHK',//00000000
-//            ]);
+        User::create(
+            [//管理員
+                'type'=>0,
+                'name'=>'admin',
+                'email'=>'admin@gmail.com',
+                'password'=>'$2y$10$EfHSnowZ1S0gsGJjVV4v9.yXBKH9SSfGGvFgghVi2DtUasju/UQHK',//00000000
+            ]);
         User::factory(
             [//學生
                 'type'=>1,
@@ -33,13 +33,13 @@ class UserSeeder extends Seeder
                 'email'=>'student@gmail.com',
                 'password'=>'$2y$10$EfHSnowZ1S0gsGJjVV4v9.yXBKH9SSfGGvFgghVi2DtUasju/UQHK',//00000000
             ])->has(Student::factory(1))->create();
-//        User::create(
-//            [//教師
-//                'type'=>2,
-//                'name'=>'teacher',
-//                'email'=>'teacher@gmail.com',
-//                'password'=>'$2y$10$EfHSnowZ1S0gsGJjVV4v9.yXBKH9SSfGGvFgghVi2DtUasju/UQHK',//00000000
-//            ]);
+        User::create(
+            [//教師
+                'type'=>2,
+                'name'=>'teacher',
+                'email'=>'teacher@gmail.com',
+                'password'=>'$2y$10$EfHSnowZ1S0gsGJjVV4v9.yXBKH9SSfGGvFgghVi2DtUasju/UQHK',//00000000
+            ])->has(Teacher::factory(1))->create();
         User::factory(1)->has(Student::factory(1))->create();
         User::factory(1)->has(Teacher::factory(1))->create();
     }
